ded77b6e534fc945ce61a39a519e22467322e51e
AvivaDrozdin <dro.aviva@outlook.com>
2021-03-07 21:26:36 +0200

    sunday first update

---
+++ Week_9/Day_1/Exercise/film_project/film_app/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .urls import *
+from .models import *
+from .forms import *
+from django.views.generic import *
+from django.urls import reverse_lazy
+
+# Create your views here.
+
+def homepage(request):
+    return render (request, "homepage.html")
+
+
+class AddFilm(CreateView):
+    form_class = AddFilmForm
+    template_name = 'film/add_film.html'
+    success_url = reverse_lazy('homepage')
+
+class AddDirector(CreateView):
+    form_class = AddDirectorForm
+    template_name = 'add_director.html'
+    success_url = reverse_lazy('homepage')
