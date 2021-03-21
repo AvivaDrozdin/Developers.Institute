b1f46ab5953e0e9f48698178e9617edfa9a8574d
AvivaDrozdin <dro.aviva@outlook.com>
2021-03-21 11:19:59 +0200

    btre update

---
+++ Week_10/BTRE/btre/listings/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from .models import Listing
+# Register your models here.
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'is_published', 'price', 'city', 'realtor',)
+    list_display_links = ('id', 'title',)
+    list_filter = ('realtor', 'city',)
+    list_editable = ('is_published',)
+    search_fields = ('title', 'description', 'address', 'city', 'state', 'zipcode', 'price', 'sqft', 'bedrooms', 'bathrooms',)
+    list_per_page = 25
+
+
+
+admin.site.register(Listing, ListingAdmin)
+
+
--- Week_10/card_game/acc_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AccAppConfig(AppConfig):
-    name = 'acc_app'
