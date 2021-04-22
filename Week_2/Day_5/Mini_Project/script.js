c54c13ec4481b047016b8f29928452d9ebe0653c
AvivaDrozdin <dro.aviva@outlook.com>
2021-04-22 18:56:39 +0300

    profile update - tbc (profile by pk)

--- Week_2/Day_5/Mini_Project/script.js
+++ Week_2/Day_5/Mini_Project/script.js
@@ -2,19 +2,18 @@ let display = document.getElementById("display"); // html source
 
 let arr = [];    // make new empty array to push our signs into
 
-function my_f(sign){ //new function - same name as my signs
-    arr.push(sign); // we push each sign into the array
-    console.log(arr);
+function my_f(sign){ 
+    arr.push(sign); 
+    // console.log(arr);
     let str = arr.join('');
     display.innerHTML=str;
 }
 
 function calculate(){ //new function to calculate our arrow
-    let str = arr.join(''); // new variable str that joins our array together
-    console.log(str);
-
-    let calc = eval(str); // new variable calc that calculates our variable str (that has the array inside)
-    console.log(calc);  
+    let str = arr.join(''); 
+    // console.log(str);
+    let calc = eval(str); 
+    // console.log(calc);  
     display.innerHTML=display.innerHTML+'='+calc;
 }
 
